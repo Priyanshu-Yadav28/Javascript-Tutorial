2918fb0b35f2a46cced30a32e669125be9bdbd0a
priyanshuYadav <priyanshuyadav28027@gmail.com>
2023-01-17 22:32:27 +0530

    02_dataTypes.js added

---
+++ 02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3)  //this will give error becasue we are using node js, not browser
+
+console.log(3 + 3)
+
+
+let name = "Priyanshu"
+let age = 21
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53 
+// bigint
+// string => "this is string"
+// boolean => true or false 
+// null => standalone value as well as datatype
+// undefined => variable declared but no variable
+// symbol => unique 
+
+// object 
+
+
+console.log(typeof "Priyanshu");
+console.log(typeof 21);
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
